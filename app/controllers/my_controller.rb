4c96c59a5f29f448788b5bbcddb744e1163464a0
Athar-Jamil-Raihan <atharjamil9@gmail.com>
2018-11-06 10:55:41 +0600

    create some new pages

--- app/controllers/my_controller.rb
+++
@@ -1,4 +0,0 @@
-class MyController < ApplicationController
- def nahi
- end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'my#nahi'
+  root to: 'pages#index'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 end
